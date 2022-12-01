625f24501e73e87ad352479990cf3a2ff7fb925b
Rick Clark <rick.clark@complyadvantage.com>
2022-12-01 15:41:12 +0000

    Add example file

---
+++ src/myFile.ts
@@ -0,0 +1,7 @@
+export default () => {
+  var object = {
+    a: '1', b: "2", c: 3, d: {subD:  [{arrayInD: 'more stuff here'}], subDpart2: {an: "I use double quotes for some reason!"}}
+}
+
+      console.log('My identation is wrong', object)
+};
